3dfb9e0d8be4e344643fe17960a2d73728a947d7
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-06-29 17:11:51 +0100

    Add board class

--- solver.py
+++ solver.py
@@ -8,9 +8,26 @@ class Piece:
     def __str__(self) -> str:
         return self.team.name
 
+class Board:
+    def __init__(self, width: int, height: int):
+        self.pieces = [[None] * width] * height
+        self.width = width
+        self.height = height
+    def __str__(self) -> str:
+        mystring: str = ''
+        for row in self.pieces:
+            for piece in row:
+                if(piece is None):
+                    mystring += ' '
+                else:
+                    mystring += piece
+            mystring += '\n'
+        return mystring
 
 def main() -> None:
     piece = Piece(Team.X)
     print(piece)
+    board = Board(3,3)
+    print(board)
 
 main()
