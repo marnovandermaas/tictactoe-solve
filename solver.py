b20ab3fd4e4da6ba0dbfe852485f4c9b1454b29e
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-06-29 18:14:28 +0100

    Implement get winner function

--- solver.py
+++ solver.py
@@ -38,20 +38,29 @@ class Board:
                 mystring += str(piece)
             mystring += '|\n'
         mystring += '-' * (self.dimension + 2)
+        mystring += '\nWinner: ' + self.get_winner().name
         return mystring
     def set_piece(self, column: int, row: int, team: Team) -> None:
         if(column >= self.dimension or row >= self.dimension):
             sys.exit()
         self.pieces[row][column].team = team
-    def get_winner(self) -> Piece:
-        # Check diagonal
-        pass
+    def get_winner(self) -> Team:
+        for line in self.pieces:
+            team = line[0].team
+            for idx in range(self.dimension):
+                if line[idx].team is not team:
+                    team = Team.E
+            if team is not Team.E:
+                return team
+        return Team.E
 
 def main() -> None:
     board = Board(3)
     board.set_piece(0, 1, Team.X)
     board.set_piece(1, 1, Team.O)
     board.set_piece(0, 2, Team.X)
+    print(board)
+    board.set_piece(0, 0, Team.X)
     print(board)
 
 main()
